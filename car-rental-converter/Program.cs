f31e2b8846bb329c52503b27aa57d14d2577d412
schneidermichael <schneidermichael@outlook.com>
2022-03-26 23:25:03 +0100

    feat: added new method Login + Authorization

--- car-rental-converter/Program.cs
+++ car-rental-converter/Program.cs
@@ -1,5 +1,9 @@
 using grpc_currency_converter.Services;
+using grpc_currency_converter.Utility;
 using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +19,29 @@ builder.Services.AddAuthentication(CertificateAuthenticationDefaults.Authenticat
         options.AllowedCertificateTypes = CertificateTypes.All;
     });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateActor = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = CurrencyConverterUtility.SecurityKey
+            };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    {
+        policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireClaim(ClaimTypes.Name);
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +49,7 @@ var app = builder.Build();
 app.UseRouting();
 
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGrpcService<CurrencyConverterImpl>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. ");
--- car-rental-converter/Services/CurrencyConverterImpl.cs
+++ car-rental-converter/Services/CurrencyConverterImpl.cs
@@ -2,6 +2,7 @@ using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using grpc_currency_converter.Utility;
 using GrpcCurrencyConverter;
+using Microsoft.AspNetCore.Authorization;
 
 namespace grpc_currency_converter.Services
 {
@@ -9,22 +10,26 @@ namespace grpc_currency_converter.Services
     {
         public CurrencyConverterImpl()
         {
-
+  
         }
-
+        [Authorize]
         public override Task<ListOfCurrenciesResponse> ListOfCurrencies(Empty request, ServerCallContext context)
         {
             return Task.FromResult(CurrencyConverterUtility.GetListOfCurrenciesResponse());
         }
 
+        [Authorize]
         public override Task<CurrencyPerSymbolResponse> CurrencyPerSymbol(CurrencyPerSymbolRequest request, ServerCallContext context)
         {
             return Task.FromResult(CurrencyConverterUtility.GetCurrencyPerSymbol(request.Symbol));
         }
 
+        [Authorize]
         public override Task<CalculatingCrossCurrencyResponse> CalculatingCrossCurrency(CalculatingCrossCurrencyRequest request, ServerCallContext context)
         {
             return Task.FromResult(CurrencyConverterUtility.GetCalculatingCrossCurrency(request));
         }
+
+        public override Task<LoginResponse> Login(LoginRequest request, ServerCallContext context) => Task.FromResult(CurrencyConverterUtility.GetLogin(request));
     }
 }
